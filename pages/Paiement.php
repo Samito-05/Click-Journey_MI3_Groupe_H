0eb472a834f2a23b37a70ea7606f053912e84e5e
Hugotpx <troupeaudebananes0@gmail.com>
2025-03-12 11:22:54 +0100

    debut paiement

---
+++ pages/Paiement.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Paiement</title>
+    <link rel="stylesheet" type="text/css" href="../style.css"> 
+</head>
