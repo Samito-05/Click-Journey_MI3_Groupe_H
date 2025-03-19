02c622a542f0cfb3b46ac76ffd5258072f430b55
Hugotpx <troupeaudebananes0@gmail.com>
2025-03-19 15:58:56 +0100

    Update Paiement.php

--- pages/Paiement.php
+++ pages/Paiement.php
@@ -7,78 +7,17 @@ session_start();
     <head>
         <meta charset="utf-8">
         <title>Paiement</title>
-        <link rel="stylesheet" type="text/css" href="../style.css"> 
+        <link rel="stylesheet" type="text/css" href="../style.css">
+        <link rel="stylesheet" type="text/css" href="../style.css?ver=<?php echo rand(); ?>">
     </head>
     <body>
-        <header class="Entete">
-
-            <?php if (!isset($_SESSION['email'])): ?>
-                <!-- When logged out -->
-                <div class="logo_petit">
-                    <a href="../index.php">
-                        <img src="../Images/logo.jpg" alt="PeakExplorer logo">
-                    </a>
-                </div>
-                <ul class="Haut_Page">
-                    <li class="inactive"><a href="../index.php">Accueil</a></li>
-                    <li class="active"><a href="../pages/A_Propos.php">À Propos</a></li>
-                </ul>
-                <div class="profile">
-                    <abbr title="Connexion/Inscription">
-                        <a href="../pages/Connexion.php">
-                            <img src="../Images/profile.png" alt="Profil">
-                        </a>
-                    </abbr>
-                </div>
-            <?php elseif ($_SESSION['email'] === "admin@peakexplorer.com"): ?>
-                <!-- When Admin -->
-                <div class="logo_petit">
-                    <a href="../index.php">
-                        <img src="../Images/logo.jpg" alt="PeakExplorer logo">
-                    </a>
-                </div>
-                <ul class="Haut_Page">
-                    <li class="inactive"><a href="../index.php">Accueil</a></li>
-                    <li class="inactive"><a href="../pages/Sejours.php">Séjours</a></li>
-                    <li class="active"><a href="../pages/A_Propos.php">À Propos</a></li>
-                </ul>
-                <div class="profile">
-                    <abbr title="Mon Profile">
-                        <a href="../pages/profile.php">
-                            <img src="../Images/profile.png" alt="Profil">
-                        </a>
-                    </abbr>
-                    <abbr title="Gestion admin">
-                        <a href="../pages/verif_admin.php">
-                            <img src="../Images/admin.png" alt="Admin">
-                        </a>
-                    </abbr>
-                </div>
-            <?php else: ?>
-                <!-- When Logged in as normal user -->
-                <div class="logo_petit">
-                    <a href="../index.php">
-                        <img src="../Images/logo.jpg" alt="PeakExplorer logo">
-                    </a>
-                </div>
-                <ul class="Haut_Page">
-                    <li class="inactive"><a href="../index.php">Accueil</a></li>
-                    <li class="inactive"><a href="../pages/Sejours.php">Séjours</a></li>
-                    <li class="active"><a href="../pages/A_Propos.php">À Propos</a></li>
-                </ul>
-                <div class="profile">
-                    <abbr title="Mon Profile">
-                        <a href="../pages/profile.php">
-                            <img src="../Images/profile.png" alt="Profil">
-                        </a>
-                    </abbr>
-                </div>
-            <?php endif; ?>
-        </header>
+        <?php
+            require('../php/header.php')
+        ?>
         <main>
             <fieldset class="field_paiement">
                 <legend class="legend_paiement">Paiement</legend>
-                <form class="form_paiement" method="post" action="../php/paiement.php">
+                <form class="form_paiement" action="../php/paiement.php" method="post">
                     <div class="div_paiement">
                         <label for="prenom">Prénom:</label>
                     </div>
@@ -97,13 +36,13 @@ session_start();
                         <label for="numero_carte">Numéro de carte:</label>
                     </div>
                     <div>
-                        <input type="text" id="numero_carte" name="numero_carte" class="champ_paiement" required>
+                        <input type="number" id="numero_carte" name="numero_carte" class="champ_paiement" required>
                     </div>
                     
                     <div class="div_paiement">
-                        <label for="date_expiration">Date d'expiration:</label>
+                        <label for="date_expiration">Date d'expiration (max. 2100):</label>
                     </div>
-                    <div>
+                    <div class="div_paiement">
                         <input type="text" id="date_expiration" name="date_expiration" placeholder="MM/AAAA" class="champ_paiement" maxlength="7" pattern="(0[1-9]|1[0-2])/20(2[5-9]|[3-9][0-9]|100)" required oninput="formatDate(this)">
                     </div>
                     <script>
@@ -120,16 +59,14 @@ session_start();
                         <label for="cvv">CVV:</label>
                     </div>
                     <div>
-                        <input type="text" id="cvv" name="cvv" class="champ_paiement" required>
+                        <input type="text" id="cvv" name="cvv" class="champ_paiement" maxlength="3" placeholder="ex: 123" required>
                     </div>
                     <button type="submit" class="boutton_paiement">Payer</button>
                 </form>
         </main>
         
         <?php
-
             require('../php/footer.php')
-
         ?>
     </body>
 </html>
