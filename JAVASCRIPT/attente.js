dd84da5a7dd796beb2eacdc5faa2df522ddb4620
Samito-05 <procoppesam@gmail.com>
2025-05-04 06:44:08 +0200

    finalisation attente

--- JAVASCRIPT/attente.js
+++ JAVASCRIPT/attente.js
@@ -1,6 +1,15 @@
 function attente(button) {
-        button.disabled = true;
-        setTimeout(() => {
-            button.disabled = false;
-        }, 2500);
+    const form = button.closest('form');
+    const select = form.querySelector('select');
+    const selectedOption = select.options[select.selectedIndex];
+
+    selectedOption.textContent = "Veuillez patienter...";
+    button.disabled = true;
+    select.disabled = true;
+
+    setTimeout(() => {
+        button.disabled = false;
+        select.disabled = false;
+        form.submit();
+    }, 2500);
 }
